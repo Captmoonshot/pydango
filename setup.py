7ed2d6b5d18000922b4d69615b38f2fd334c35b7
Captmoonshot <gygantor@protonmail.com>
2021-02-04 03:50:07 -0800

    changed install_requires of setup.py

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
     ],
     packages=["pydango"],
     include_package_data=True,
     install_requires=[
             "sqlalchemy==1.3.22",
-            "sqlalchemy-utils==0.36.8", 
+            "sqlalchemy-utils==0.36.6", 
             "psycopg2-binary==2.8.6",
             "passlib==1.7.4",
             "python-dateutil==2.8.1",    
